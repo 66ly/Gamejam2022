89efaff00ed502acf39a0b6f85dc078c6b5ee746
Tyler Zhang <tianle.zhang@hotmail.com>
2023-11-04 16:47:53 -0700

    Stick object 2.0

--- Assets/Scripts/StickObject.cs
+++ Assets/Scripts/StickObject.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 
 public class StickObject : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 360f;
-    public float stickThreshold = 0.5f;  // 粘附阈值
+    public bool isStucking = false;
 
-    public bool isStuck = false;
-
-
+    private Dictionary<string, FixedJoint2D> stickObjects = new Dictionary<string, FixedJoint2D>();
     void Update()
     {
         // if (isStuck) return;  // 如果已经粘附，不再处理玩家输入
@@ -27,12 +23,27 @@ public class StickObject : MonoBehaviour
         // Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
         // transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         // }
+
+        if (Input.GetKeyDown(KeyCode.Z) && !isStucking) 
+        {
+            Debug.Log("Sticking Mode");
+            isStucking = true;
+        } else if (Input.GetKeyDown(KeyCode.Z) && isStucking)
+        {
+            Debug.Log("Exit Sticking Mode");
+            isStucking = false;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         // Your code here
-        Debug.Log("col " + collision);
+        Debug.Log("Collision Enter with " + collision.gameObject.name);
+
+        if (!isStucking) 
+        {
+            return;
+        }
 
         if (collision.transform.gameObject.tag == "Block")
         {
@@ -43,7 +54,29 @@ public class StickObject : MonoBehaviour
             // conects the joint to the other object
             joint.connectedBody = collision.transform.GetComponentInParent<Rigidbody2D>();
             // Stops objects from continuing to collide and creating more joints
-            joint.enableCollision = false;
+            joint.enableCollision = true;
+
+            stickObjects.TryAdd(collision.gameObject.name, joint);
+        }
+    }
+
+    // For 2D physics
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("collision exited with " + collision.gameObject.name);
+
+        if (!isStucking)
+        {
+            return;
+        }
+        // code to execute on collision exit
+
+        if (stickObjects.ContainsKey(collision.gameObject.name))
+        {
+            FixedJoint2D fixedjoint = stickObjects[collision.gameObject.name];
+            fixedjoint.enabled = false;
+            Destroy(fixedjoint);
+            stickObjects.Remove(collision.gameObject.name);
         }
     }
 }
